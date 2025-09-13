22453e82e0c9ed613a957cee4d588e5e5357fb47
taisei023 <tm.lovin8@gmail.com>
2025-09-13 11:52:08 +0900

    #1 リリィ用に仕様変更

--- commands/hello.py
+++ commands/hello.py
@@ -1,8 +1,7 @@
 import discord
-from config import ADMIN_ROLE_ID
 
 async def setup(bot):
     
-    @bot.tree.command(name="hello", description="このコマンドによってノワールの生存確認ができます")
+    @bot.tree.command(name="hello", description="このコマンドによってリリィの生存確認ができます")
     async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message("職務遂行中です！")
+        await interaction.response.send_message("職務遂行中よ！")
--- config.py
+++ config.py
@@ -4,11 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-VOICE_CHANNEL_ID = 847514073964740679
-
-ADMIN_ROLE_ID = 946432769242845224
-
-MEIBO_CHANNEL_ID = 847686946268512287
+VOICE_CHANNEL_ID = 860122545381572608
 
 # 女性の場合: ja-JP-NanamiNeural, 男性の場合: ja-JP-KeitaNeural
 VOICE_MODEL = "ja-JP-NanamiNeural"
